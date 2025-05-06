d31e96d196b86268e5ad8c3a420d615d8a45d9df
rsgGit <rsgeorge2000@gmail.com>
2025-05-06 13:43:29 +0300

    updated app

--- job_search_backend/app.py
+++ job_search_backend/app.py
@@ -13,25 +13,25 @@ app = Flask(__name__)
 def home():
     return "Hellooo from Railway!"
 
-@app.route('/get-countries', methods=['GET'])
-def get_countries():
-    return jsonify(get_all_countries())
+# @app.route('/get-countries', methods=['GET'])
+# def get_countries():
+#     return jsonify(get_all_countries())
 
-@app.route('/load-jobs', methods=['GET'])
-def load_jobs():
-    # timeout = aiohttp.ClientTimeout(total=60)
-    keyword = request.args.get('keyword', default=None, type=str)
-    location = request.args.get('location', default=None, type=str)
-    date_posted = request.args.get('date_posted', default=None, type=str)
-    page = int(request.args.get('page', 1))
-    if keyword=="": keyword = None
-    if location=="": location = None
-    if date_posted=="": date_posted = None
-    page_data = get_jobs_with_sponsorship(keyword, location, date_posted, page, 50)
-    jobs = page_data['data']
-    if isinstance(page_data.get('data'), pd.DataFrame):
-        page_data['data'] = page_data['data'].to_dict(orient='records')
-    return jsonify(page_data)
+# @app.route('/load-jobs', methods=['GET'])
+# def load_jobs():
+#     # timeout = aiohttp.ClientTimeout(total=60)
+#     keyword = request.args.get('keyword', default=None, type=str)
+#     location = request.args.get('location', default=None, type=str)
+#     date_posted = request.args.get('date_posted', default=None, type=str)
+#     page = int(request.args.get('page', 1))
+#     if keyword=="": keyword = None
+#     if location=="": location = None
+#     if date_posted=="": date_posted = None
+#     page_data = get_jobs_with_sponsorship(keyword, location, date_posted, page, 50)
+#     jobs = page_data['data']
+#     if isinstance(page_data.get('data'), pd.DataFrame):
+#         page_data['data'] = page_data['data'].to_dict(orient='records')
+#     return jsonify(page_data)
 
 
 if __name__ == "__main__":
