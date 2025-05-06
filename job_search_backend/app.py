89b3faeb4baa7ee7aa2dbdbf8ace2d2aa954c99e
rsgGit <rsgeorge2000@gmail.com>
2025-05-06 13:02:20 +0300

    made app.py basic to see if it works

--- job_search_backend/app.py
+++ job_search_backend/app.py
@@ -1,40 +1,12 @@
-from flask import Flask, jsonify, request
-from flask_cors import CORS
-from jobspy import scrape_jobs
-
-import pandas as pd
-import asyncio
-import aiohttp
-# from .db_utils import get_all_countries, get_jobs_with_sponsorship
-import os
+from flask import Flask
 
 app = Flask(__name__)
-CORS(app)
 
-@app.route('/')
-def hello():
+@app.route("/")
+def home():
     return "Hello from Railway!"
 
-# @app.route('/get-countries', methods=['GET'])
-# def get_countries():
-#     return jsonify(get_all_countries())
-
-# @app.route('/load-jobs', methods=['GET'])
-# def load_jobs():
-#     # timeout = aiohttp.ClientTimeout(total=60)
-#     keyword = request.args.get('keyword', default=None, type=str)
-#     location = request.args.get('location', default=None, type=str)
-#     date_posted = request.args.get('date_posted', default=None, type=str)
-#     page = int(request.args.get('page', 1))
-#     if keyword=="": keyword = None
-#     if location=="": location = None
-#     if date_posted=="": date_posted = None
-#     page_data = get_jobs_with_sponsorship(keyword, location, date_posted, page, 50)
-#     jobs = page_data['data']
-#     if isinstance(page_data.get('data'), pd.DataFrame):
-#         page_data['data'] = page_data['data'].to_dict(orient='records')
-#     return jsonify(page_data)
-
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
